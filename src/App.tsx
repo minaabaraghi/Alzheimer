7d4f10ccb23c4e3fd426fcba2482e43b72b1a777
Mina Abaraghi <M.Abaraghi@kharazmico.local>
2022-07-27 12:10:07 +0430

    added login file and installed router pcj

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import Alzheimer from './components/Alzheimer';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Alzheimer/>
+        <Login/>
       </header>
     </div>
   );
