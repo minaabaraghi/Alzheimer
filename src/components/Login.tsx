21fff0aa2192c2dd8a4c9bbf1c09d6a8bd603832
Mina Abaraghi <M.Abaraghi@kharazmico.local>
2022-08-01 18:22:24 +0430

    convert class to func component and get user and pass from form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,33 +1,35 @@
-import React from "react";
-import login from "../services/login"
+import React,{useState} from "react";
+import { toast } from "react-toastify";
+import login from "../services/login";
 
-class LoginComponent extends React.Component {
-  constructor(props: any) {
-    super(props);
-  }
-  login() {
-    login("abbasi", "123").then(result => {
+//todo: Convert to funcation base
+export default function LoginComponent() {
+  //todo: get username and password from form 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const login1 = ()=>{
+    login(email, password).then(result => {
+      localStorage.setItem('token', result);
       console.log(result);
+      //todo: why does toastify not work correctly?
+      toast.error('ورود موفقیت آمیز')
     })
   }
-  render(): React.ReactNode {
-    return (
-      <main className="form-signin w-100 m-auto">
-        <form>
-          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-          <div className="form-floating">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-            <label htmlFor="floatingInput">Email address</label>
-          </div>
-          <div className="form-floating">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-            <label htmlFor="floatingPassword">Password</label>
-          </div>
-          <button className="w-100 btn btn-lg btn-primary" type="button" onClick={this.login}>Sign in</button>
-        </form>
-      </main>
-    )
-  }
+  return (
+    <main className="form-signin w-100 m-auto">
+    <form>
+      <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+      <div className="form-floating">
+        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" onChange={e => setEmail(e.target.value)} />
+        <label htmlFor="floatingInput">Email address</label>
+      </div>
+      <div className="form-floating">
+        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+        <label htmlFor="floatingPassword">Password</label>
+      </div>
+      <button className="w-100 btn btn-lg btn-primary" type="button" onClick={login1}>Sign in</button>
+    </form>
+  </main>
+  )
 }
 
-export default LoginComponent;
--- src/services/login.tsx
+++ src/services/login.tsx
@@ -3,6 +3,7 @@ import { AxiosRequestConfig } from "axios"
 import axiosInstance from "./axiosInstance"
 
 const login = (username: string, password: string): Promise<any> => {
+    //todo: save jwt to localstorage then return true/false
     const reqConfig: AxiosRequestConfig = {
         method: 'POST',
         url: 'auth',
